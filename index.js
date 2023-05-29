91ea1066b6fcab2b23b83c390fa7923e1534a428
radkam2000 <radkam2000@protonmail.com>
2023-05-29 11:48:37 +0200

    change api function to use fetch, data parsing

--- index.js
+++ index.js
@@ -6,53 +6,57 @@ app.get("/", (req, res) => {
 	res.send("Aplikacja dziala");
 });
 
+let cryptoDataAfterParsing;
+let stockDataAfterParsing;
+
 const getStockData = () => {
-	https.get(
-		`https://api.stlouisfed.org/fred/series/observations?series_id=NASDAQ100&api_key=${process.env.STOCK_API_KEY}&file_type=json`,
-		(res) => {
-			let body = "";
-
-			res.on("data", (chunk) => {
-				body += chunk;
-			});
-
-			res.on("end", () => {
-				try {
-					let json = JSON.parse(body.prices);
-					console.log(json);
-					// do something with JSON
-				} catch (error) {
-					console.error(error.message);
+	fetch(
+		`https://api.stlouisfed.org/fred/series/observations?series_id=NASDAQ100&api_key=${process.env.STOCK_API_KEY}&file_type=json`
+	)
+		.then((res) => res.json())
+		.then((json) => {
+			let tmpObservations = [];
+			json.observations.forEach((element) => {
+				if (element.hasOwnProperty("realtime_start")) {
+					delete element["realtime_start"];
 				}
+				if (element.hasOwnProperty("realtime_end")) {
+					delete element["realtime_end"];
+				}
+				tmpObservations.push([
+					element["date"],
+					parseFloat(element["value"]),
+				]);
 			});
-		}
-	);
+			stockDataAfterParsing = tmpObservations;
+			console.log(stockDataAfterParsing[0]);
+		});
 };
 
 const getCryptoData = () => {
-	https.get(
-		`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max`,
-		(res) => {
-			let body = "";
-
-			res.on("data", (chunk) => {
-				body += chunk;
+	fetch(
+		`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max`
+	)
+		.then((res) => res.json())
+		.then((json) => {
+			json.prices.forEach((element) => {
+				tmpDate = new Date(element[0]);
+				element[0] =
+					tmpDate.getFullYear() +
+					"-" +
+					(tmpDate.getMonth() + 1 < 10
+						? "0" + (tmpDate.getMonth() + 1)
+						: tmpDate.getMonth() + 1) +
+					"-" +
+					tmpDate.getDate();
 			});
-
-			res.on("end", () => {
-				try {
-					let json = JSON.parse(body);
-					console.log(json.prices);
-					// do something with JSON
-				} catch (error) {
-					console.error(error.message);
-				}
-			});
-		}
-	);
+			cryptoDataAfterParsing = json.prices;
+			console.log(cryptoDataAfterParsing[0]);
+		});
 };
 
 app.get("/data", (req, res) => {
+	getStockData();
 	getCryptoData();
 	res.send("Pobrano dane");
 });
