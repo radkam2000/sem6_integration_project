dbc764424f0765a1a11cf2e0b7ba3d3a6211393e
nataliaprazmo <prazmonatalia@gmail.com>
2023-06-05 17:18:33 +0200

    added LineChart, made charts for both types of data, fetched data from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,126 @@
 import { useState } from "react";
 import "./App.css";
-import Test from "./components/Test";
-import { CryptoData } from "./CryptoData";
+import axios from "axios";
 import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
 
 function App() {
-    const [testData, setTestData] = useState(CryptoData);
-    const size = Object.values(testData).length;
-    const [dates, setDates] = useState([]);
-    const [prices, setPrices] = useState([]);
-    for (var i = 0; i < 10; i++) {
-        dates.push(testData[i][0]);
-        prices.push(testData[i][1]);
-    }
-    const [chartData, setChartData] = useState({
-        labels: dates,
-        datasets: [
-            {
-                label: "Price",
-                data: prices,
-                backgroundColor: [
-                    "#9c92ac",
-                    "#f5b1aa",
-                    "#8fd3f4",
-                    "#c6e2e9",
-                    "#e4c1f9",
-                ],
-                borderColor: "#c5c5c5",
-                borderWidth: 2,
-                hoverBackgroundColor: "#ff9a00",
-            },
-        ],
-    });
-    return (
-        <div className="App">
-            <Test data={testData} />
-            <div style={{ width: 800 }}>
-                <BarChart chartData={chartData} />
-            </div>
-        </div>
-    );
+	const [stockData, setStockData] = useState([]);
+	const [cryptoData, setCryptoData] = useState([]);
+	const [pobrano, setPobrano] = useState(false);
+	const handleGetData = async () => {
+		try {
+			const config = {
+				method: "get",
+				url: "http://localhost:5000/api/data_api",
+				headers: { "Content-Type": "application/json" },
+			};
+			const { data: res } = await axios(config);
+			setStockData(res.stock);
+			setCryptoData(res.crypto);
+			setPobrano(true);
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				window.location.reload();
+			}
+		}
+	};
+	const [isButtonClicked, setButtonClicked] = useState(false);
+
+	var dates = [];
+	var prices = [];
+	const [size, setSize] = useState(0);
+	const [chartData, setChartData] = useState({});
+	const [chartData2, setChartData2] = useState({});
+	const groupData = () => {
+		setSize(Object.values(cryptoData).length);
+		for (var i = 0; i < 10; i++) {
+			dates.push(cryptoData[i][0]);
+			prices.push(cryptoData[i][1]);
+		}
+		setChartData({
+			labels: dates,
+			datasets: [
+				{
+					label: "Crypto prices",
+					data: prices,
+					backgroundColor: ["#9c92ac"],
+					borderColor: "#c5c5c5",
+					borderWidth: 2,
+					hoverBackgroundColor: "#ff9a00",
+				},
+			],
+		});
+		dates = [];
+		prices = [];
+		setSize(Object.values(stockData).length);
+		for (var j = 0; j < 10; j++) {
+			dates.push(stockData[i][0]);
+			prices.push(stockData[i][1]);
+		}
+		setChartData2({
+			labels: dates,
+			datasets: [
+				{
+					label: "Stock prices",
+					data: prices,
+					backgroundColor: ["#f5b1aa"],
+					borderColor: "#c5c5c5",
+					borderWidth: 2,
+					hoverBackgroundColor: "#ff9a00",
+				},
+			],
+		});
+		setButtonClicked(true);
+	};
+	return (
+		<div className="App">
+			<h1>Frontend</h1>
+			<button onClick={handleGetData}>Pobierz dane</button>
+			pobrano: {pobrano ? "tak" : "nie"}
+			<p></p>
+			<button onClick={groupData}>Pokaż wykresy</button>
+			<p>
+				<h4>Wykresy kryptowaluty</h4>
+				{pobrano && isButtonClicked ? (
+					<LineChart chartData={chartData} />
+				) : (
+					""
+				)}
+				{pobrano ? (
+					isButtonClicked ? (
+						<BarChart chartData={chartData} />
+					) : (
+						"nie kliknięto"
+					)
+				) : (
+					"nie pobrano danych"
+				)}
+			</p>
+			<p></p>
+			<p>
+				<h4>Wykresy giełda</h4>
+				{pobrano && isButtonClicked ? (
+					<LineChart chartData={chartData2} />
+				) : (
+					""
+				)}
+				{pobrano ? (
+					isButtonClicked ? (
+						<BarChart chartData={chartData2} />
+					) : (
+						"nie kliknięto"
+					)
+				) : (
+					"nie pobrano danych"
+				)}
+			</p>
+		</div>
+	);
 }
 
 export default App;
