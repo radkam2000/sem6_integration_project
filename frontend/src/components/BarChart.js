80f7066c2cb55d9738a2cea45468e1a48d8e1df3
nataliaprazmo <prazmonatalia@gmail.com>
2023-06-05 17:18:33 +0200

    added LineChart, made charts for both types of data, fetched data from api

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 function BarChart({ chartData }) {
-    return <Bar data={chartData} />;
+	return <Bar data={chartData} />;
 }
 export default BarChart;
