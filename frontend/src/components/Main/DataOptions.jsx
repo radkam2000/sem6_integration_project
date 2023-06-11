03b7c848204363384121a534d83cbbe879efc4cd
nataliaprazmo <prazmonatalia@gmail.com>
2023-06-11 12:05:05 +0200

    added export download on front

---
+++ frontend/src/components/Main/DataOptions.jsx
@@ -0,0 +1,50 @@
+const DataOptions = (props) => {
+	return (
+		<div className={styles.options}>
+			<h2>Zarządzaj danymi</h2>
+			<div className={styles.opt}>
+				<p className={styles.opt_container}>
+					<label>API</label>
+					<button
+						className={styles.optionStyle}
+						onClick={handleGetData}
+					>
+						Pobierz dane
+					</button>
+				</p>
+				<p className={styles.opt_container}>
+					<label htmlFor="fileType">Plik</label>
+					<select
+						className={styles.selectStyle}
+						id="fileType"
+						onChange={(e) => setFileType(e.target.value)}
+					>
+						<option value="xml">XML</option>
+						<option value="json">JSON</option>
+					</select>
+					<input
+						type="file"
+						id="file"
+						ref={inputFile}
+						style={{ display: "none" }}
+						onChange={handleFileChange}
+					/>
+					<button className={styles.optionStyle} onClick={chooseFile}>
+						Importuj
+					</button>
+					<button
+						className={styles.optionStyle}
+						onClick={handleExport}
+					>
+						Eksportuj
+					</button>
+				</p>
+				<p className={styles.opt_container}>
+					<button onClick={saveToDB} className={styles.optionStyle}>
+						Zapisz kopię do bazy danych
+					</button>
+				</p>
+			</div>
+		</div>
+	);
+};
