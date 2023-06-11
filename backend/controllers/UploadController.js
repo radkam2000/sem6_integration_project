00ddf35c3b45a493ff1dbac09a63f3db269cef8b
radkam2000 <radkam2000@protonmail.com>
2023-06-11 16:35:37 +0200

    formatting changes

--- backend/controllers/UploadController.js
+++ backend/controllers/UploadController.js
@@ -1,6 +1,11 @@
 const arrayParser = require("../parsers/toArrayParser");
+const fs = require("fs").promises;
+const convert = require("xml-js");
 
-const XMLupload = () => {};
+const XMLupload = async () => {
+	data = await fs.readFile(`${__dirname}/../uploads/data`, "utf8");
+	console.log(convert.xml2js);
+};
 
 const JSONupload = () => {};
 
--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const multer = require("multer");
+const uploadController = require("../controllers/UploadController");
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, __dirname + "/../uploads/");
 	},
 	filename: function (req, file, cb) {
-		cb(null, file.originalname);
+		cb(null, "data");
 	},
 });
 
@@ -14,6 +15,7 @@ const uploads = multer({ storage: storage });
 
 router.post("/json", uploads.single("file"), (req, res) => {
 	try {
+		console.log(req);
 		res.status(200).send({ message: "File received succesfully" });
 	} catch (error) {
 		console.error(error);
@@ -21,8 +23,9 @@ router.post("/json", uploads.single("file"), (req, res) => {
 	}
 });
 
-router.post("/xml", uploads.single("file"), (req, res) => {
+router.post("/xml", uploads.single("file"), async (req, res) => {
 	try {
+		//await uploadController.XMLupload();
 		res.status(200).send({ message: "File received succesfully" });
 	} catch (error) {
 		console.error(error);
